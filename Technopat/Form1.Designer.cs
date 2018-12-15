f8593752480e0a2de39b2064e98c4673238d54c8
Batuhan Aktaş <aktas.batu@hotmail.com>
2018-12-15 18:18:14 +0300

    Proje dosyası ekle.

---
+++ Technopat/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Technopat
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton5 = new MetroFramework.Controls.MetroButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton4 = new MetroFramework.Controls.MetroButton();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // metroButton5
+            // 
+            this.metroButton5.Location = new System.Drawing.Point(482, 70);
+            this.metroButton5.Name = "metroButton5";
+            this.metroButton5.Size = new System.Drawing.Size(129, 41);
+            this.metroButton5.TabIndex = 23;
+            this.metroButton5.Text = "Dosyaya Kaydet";
+            this.metroButton5.UseSelectable = true;
+            this.metroButton5.Click += new System.EventHandler(this.metroButton5_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(23, 129);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(209, 589);
+            this.listBox1.TabIndex = 21;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(23, 70);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(129, 41);
+            this.metroButton1.TabIndex = 20;
+            this.metroButton1.Text = "Sayfaya Git";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.Location = new System.Drawing.Point(330, 70);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(129, 41);
+            this.metroButton3.TabIndex = 24;
+            this.metroButton3.Text = "Sonraki Sayfaya Geç";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(178, 70);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(129, 41);
+            this.metroButton2.TabIndex = 22;
+            this.metroButton2.Text = "ListBox\'a Ekle";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton4
+            // 
+            this.metroButton4.Location = new System.Drawing.Point(973, 70);
+            this.metroButton4.Name = "metroButton4";
+            this.metroButton4.Size = new System.Drawing.Size(129, 41);
+            this.metroButton4.TabIndex = 25;
+            this.metroButton4.Text = "Alternatif";
+            this.metroButton4.UseSelectable = true;
+            this.metroButton4.Click += new System.EventHandler(this.metroButton4_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(238, 129);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(864, 589);
+            this.listBox2.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1125, 739);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.metroButton4);
+            this.Controls.Add(this.metroButton5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton5;
+        private System.Windows.Forms.ListBox listBox1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private MetroFramework.Controls.MetroButton metroButton3;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton4;
+        private System.Windows.Forms.ListBox listBox2;
+    }
+}
+
---
+++ Technopat/Form1.cs
@@ -0,0 +1,138 @@
+﻿using HtmlAgilityPack;
+using MetroFramework.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Technopat
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string url = "https://www.technopat.net/video/";
+        IWebDriver drivertp = new EdgeDriver();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            drivertp.Navigate().GoToUrl(url);
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            ReadOnlyCollection<IWebElement> lists1 = drivertp.FindElements(By.CssSelector("#td-outer-wrap > div > div.td-container.td-category-container > div > div > div.td-pb-span8.td-main-content > div > div > div.meta-info-container > div.meta-info > h3 > a"));
+
+            for (int i = 0; i < lists1.Count; i++)
+            {
+                listBox1.Items.Add(lists1[i].Text);
+                listBox1.Items.Add(lists1[i].GetAttribute("href"));
+            }
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            IWebElement tplink = drivertp.FindElement(By.ClassName("td-icon-menu-right"));
+            tplink.Click();
+            //MessageBox.Show(tplink.GetAttribute("href"));
+        }
+
+        private void metroButton5_Click(object sender, EventArgs e)
+        {
+            int sayac = 1;
+            int ksayac = 2;
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.CreatePrompt = true;
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                string item = "";
+                StreamWriter Kayit = new StreamWriter(sfd.FileName);
+                string onsatir = "<DT><H3 ADD_DATE=\"1515536908\" LAST_MODIFIED=\"1531485215\">TP</H3>";
+                Kayit.WriteLine(onsatir);
+                string satir1 = "<DL><p>";
+                Kayit.WriteLine(satir1);
+                string satir2 = "<DT><H3 ADD_DATE=\"1531485215\" LAST_MODIFIED=\"1531485215\">TP1</H3>";
+                Kayit.WriteLine(satir2);
+                string satir3 = "</DL><p>";
+                Kayit.WriteLine(satir1);
+                for (int i = 0; i < listBox2.Items.Count; i += 2)
+                {
+                    if (sayac < 10)
+                    {
+                        item = "<DT><A HREF=\"" + listBox2.Items[i] + "\" ADD_DATE=\"1531485215\">" + listBox2.Items[i + 1] + "</A>";
+                        Kayit.WriteLine(item);
+                        sayac++;
+                    }
+                    else
+                    {
+                        Kayit.WriteLine(satir3);
+                        satir2 = "<DT><H3 ADD_DATE=\"1531485215\" LAST_MODIFIED=\"1531485215\">TP" + ksayac + "</H3>";
+                        Kayit.WriteLine(satir2);
+                        Kayit.WriteLine(satir1);
+                        ksayac++;
+                        sayac = 0;
+                    }
+
+                }
+                Kayit.Close();
+            }
+        }
+
+        private void ekle(string link, int iter)
+        {
+            HtmlAttribute linkk;
+            string linkiter;
+            for (int i = 1; i <= iter; i++)
+            {
+                if (i == 1)
+                {
+                    linkiter = url;
+                }
+                else
+                {
+                    linkiter = link + i.ToString();
+                }
+                //listBox1.Items.Add(linkiter);
+                var html = @linkiter;
+                HtmlWeb web = new HtmlWeb();
+                var htmlDoc = web.Load(html);
+                //HtmlNodeCollection node = htmlDoc.DocumentNode.SelectNodes("//div[@class='meta-info']//h3//a");
+
+                HtmlNodeCollection node = htmlDoc.DocumentNode.SelectNodes("//div[@class='meta-info']//h3//a");
+                int ncnt = node.Count;
+                //MessageBox.Show(ncnt.ToString());
+                for (int j = 0; j < ncnt; j++)
+                {
+                    linkk = node[j].Attributes["href"];
+                    if (!listBox1.Items.Contains(linkk.Value) && !listBox1.Items.Contains(node[j].InnerText))
+                    {
+                        listBox1.Items.Add(node[j].InnerText);
+                        listBox1.Items.Add(linkk.Value);
+                    }
+                }              
+            }
+            for (int i= listBox1.Items.Count-1;i>=0;i--)
+            {
+                listBox2.Items.Add(listBox1.Items[i]);
+            }
+        }
+
+        private void metroButton4_Click(object sender, EventArgs e)
+        {
+            string link = "https://www.technopat.net/video/page/";
+            //string son = "https://www.technopat.net/2018/07/12/cooler-master-mm530-rgb-oyuncu-faresi-incelemesi/";
+            ekle(link, 8);
+        }
+    }
+}
